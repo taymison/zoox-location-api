5f70f30541e6c4abcbbcafe7f590b001e8fde851
taymison <taymison@tagme.com.br>
2021-02-09 19:52:01 -0300

    refactor: :fire: Remove unnused files

--- src/interceptors/localize-generated-dates.interceptor.spec.ts
+++
@@ -1,7 +0,0 @@
-import { LocalizeGeneratedDatesInterceptor } from './localize-generated-dates.interceptor';
-
-describe('LocalizeGeneratedDatesInterceptor', () => {
-  it('should be defined', () => {
-    expect(new LocalizeGeneratedDatesInterceptor()).toBeDefined();
-  });
-});
--- src/pipes/validate-sort-arg.pipe.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ParseSortArgPipe } from './parse-sort-arg.pipe';
-
-describe('ParseSortArgPipe', () => {
-  it('should be defined', () => {
-    expect(new ParseSortArgPipe()).toBeDefined();
-  });
-});
